7dda5c8785bedb973025a767c6f9ac1b1e8743c1
robroche565 <111483459+robroche565@users.noreply.github.com>
2022-11-15 11:24:17 +0800

    service part
    done doing service section

--- index.php
+++ index.php
@@ -5,6 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/styles.css">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <title>Welcome Page</title>
 </head>
     <body>
@@ -22,26 +28,41 @@
             
         </header>
         
-        <div id="section1">
-            <div class="container">
+        <section id="section1">
+            <div class="container_sec1">
                 <h1>Welcome to </h1>
                 <img src="images/Spy_×_Family_logo.png">
                 <h2>Cafe Restaurant</h2>
             </div>
             
-        </div>  
+        </section>  
 
-        <div id="section2">
-            <h1>This is Service</h1>
-        </div>
+        <section id="section2">
+            <div class="services">
+                <img src="images/25g7d94guat81.png"> 
+                <div class="service-text">
+                    <h1>Service</h1>
+                    <h5>What do we do here?</h5>
+                    <p>Lorem ipsum dolor sit amet. A assumenda nesciunt eum quis enim qui dolorem 
+                        sunt 33 quaerat minima ut accusantium laborum quo iure voluptatem. Qui sint 
+                        reprehenderit qui suscipit voluptatem qui fugiat dolore. Hic voluptas neque
+                        est voluptatibus quia qui dolorem eius nam modi possimus et necessitatibus 
+                        ducimus non reiciendis voluptas. Sit earum tempore ut dolores nihil aut illum 
+                        doloribus ea unde suscipit rem tempora sunt ut impedit ipsam.</p>
 
-        <div id="section3">
+                </div>
+                
+            </div>
+            
+        </section>
+
+        <section id="section3">
             <h1>This is Waiter/Waitress</h1>
-        </div>
+        </section>
 
-        <div id="section4">
+        <section id="section4">
             <h1>This is About us</h1>
-        </div>
+        </section>
    
         
     </body>
