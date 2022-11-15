73670774bd85d8c01e839a77dabb0c46cae90466
robroche565 <111483459+robroche565@users.noreply.github.com>
2022-11-15 12:43:22 +0800

    waiter/waitress
    fixed those part

--- index.php
+++ index.php
@@ -49,19 +49,61 @@
                         est voluptatibus quia qui dolorem eius nam modi possimus et necessitatibus 
                         ducimus non reiciendis voluptas. Sit earum tempore ut dolores nihil aut illum 
                         doloribus ea unde suscipit rem tempora sunt ut impedit ipsam.</p>
+                </div>  
+            </div>        
+        </section>
 
+        <div id="section3">
+            <div class="title">
+                <h2>Waiter/Waitress</h2>
+            </div>
+            <img src="images/to be enhanced.png" style="width: 60%; display:block; margin-left:auto; margin-right:auto">
+            <div class="box">
+                <div class="card">
+                    <h5>Loid Forger</h5>
+                    <div class="pra">
+                        <p>Lorem ipsum dolor sit amet. A assumenda nesciunt eum quis enim qui dolorem 
+                        sunt 33 quaerat minima ut accusantium laborum quo iure voluptatem.</p>
+                    </div>
                 </div>
-                
+            </div>
+
+            <div class="box">
+                <div class="card">
+                    <h5>Anya Forger</h5>
+                    <div class="pra">
+                        <p>Lorem ipsum dolor sit amet. A assumenda nesciunt eum quis enim qui dolorem 
+                        sunt 33 quaerat minima ut accusantium laborum quo iure voluptatem.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="card">
+                    <h5>Yor Forger</h5>
+                    <div class="pra">
+                        <p>Lorem ipsum dolor sit amet. A assumenda nesciunt eum quis enim qui dolorem 
+                        sunt 33 quaerat minima ut accusantium laborum quo iure voluptatem.</p>
+                    </div>
+                </div>
             </div>
             
-        </section>
-
-        <section id="section3">
-            <h1>This is Waiter/Waitress</h1>
-        </section>
+        </div>
 
         <section id="section4">
-            <h1>This is About us</h1>
+            <div class="about-us">
+                    <img src="images\BK-1.png"> 
+                    <div class="about-text">
+                        <h1>Service</h1>
+                        <h5>What do we do here?</h5>
+                        <p>Lorem ipsum dolor sit amet. A assumenda nesciunt eum quis enim qui dolorem 
+                            sunt 33 quaerat minima ut accusantium laborum quo iure voluptatem. Qui sint 
+                            reprehenderit qui suscipit voluptatem qui fugiat dolore. Hic voluptas neque
+                            est voluptatibus quia qui dolorem eius nam modi possimus et necessitatibus 
+                            ducimus non reiciendis voluptas. Sit earum tempore ut dolores nihil aut illum 
+                            doloribus ea unde suscipit rem tempora sunt ut impedit ipsam.</p>
+                    </div>  
+                </div>
         </section>
    
         
