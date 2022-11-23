9a0c3c2d5ada4b231f91a99f7f5b2f26871ee7f0
robroche565 <111483459+robroche565@users.noreply.github.com>
2022-11-23 23:41:44 +0800

    added log in java script
    very nice

--- index.php
+++ index.php
@@ -8,9 +8,8 @@
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" >
+    
     <title>Welcome Page</title>
 </head>
     <body>
@@ -26,9 +25,30 @@
                     <li><a href="#section4">About Us</a></li>
                 </ul> 
             </nav>
-            <a class="log" href="#"><button>Log-In</button></a>
+                <div class="container">
+                    <a href="#" class="button" id="button">Login</a>
+                </div>
+                  
             
         </header>
+        <div class="popup">
+            <div class="popup-container">
+                <img src="images/close-icon-29.png" alt="close" class="close">
+                <img src="images/Spy_×_Family_logo.png" alt="user" class="logopop">
+                <input type="text" placeholder="Username">
+                <input type="password" placeholder="Password">
+                <a href="#" class="button" >Login</a>
+            </div>
+        </div>
+        <script>
+            document.getElementById("button").addEventListener("click", function(){
+                document.querySelector(".popup").style.display="flex";
+            })
+            document.querySelector(".close").addEventListener("click", function(){
+                document.querySelector(".popup").style.display="none";
+            })
+        </script>
+        
         
         <section id="section1">
             <div class="container_sec1">
