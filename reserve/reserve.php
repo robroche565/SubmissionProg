15e55bba9e085125233433bb09f1bcdc890427ad
robroche565 <111483459+robroche565@users.noreply.github.com>
2022-11-27 15:20:23 +0800

    added form
    form

--- reserve/reserve.php
+++ reserve/reserve.php
@@ -11,6 +11,37 @@
 ?>
     
 
+    <div class="add-form-container">
+        <div class="add-form-box">
+        <h3>Reservation</h3>
+        </br> 
+            <form class="add-form" method="post" action="reserve.php">
+                <label for="date">Date of Reservation</label>
+                <input type="datetime-local" name="date" placeholder="Date" required>
+            </br> </br> </br>
+                <div>
+                    <label for="role">Choose Your Waiter/Waitress</label><br>
+                    <label class="container" for="anya">Anya Forger
+                        <input type="radio" name="server" id="anya" value="Anya Forger" >
+                        <span class="checkmark"></span>
+                    </label>
+                    <label class="container" for="yor">Yor Forger
+                        <input type="radio" name="server" id="yor" value="Yor Forger" >
+                        <span class="checkmark"></span>
+                    </label>
+                    <label class="container" for="loid">Loid Forger
+                        <input type="radio" name="server" id="loid" value="Loid Forger" >
+                        <span class="checkmark"></span>
+                    </label>
+                    <input type="submit" class="button" value="Reserve" name="res" id="res">
+                            
+                </div>
+        </div>
+        </form>
+
+    </div>
+    
+
 <?php 
     
     require_once '../includes/footer.php';
